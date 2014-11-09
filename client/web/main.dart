1dcdcf0024af2b8a7eddc11584ef40709ca5abdd
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-11-09 18:23:26 +0100

    Added support for CORS and dynamic port selection, same code runs both locally and on DartVoid without changes. Also added fix for encoding.

--- client/web/main.dart
+++ client/web/main.dart
@@ -10,11 +10,19 @@ void main() {
   String serviceUrl = "/hello";
 
   formSubmit.onClick.listen((e) {
+    // Prevent form submit
     e.preventDefault();
+
+    // Setup url (port 9090 for during development locally, otherwise use
+    // standard port 80 for production)
+    Uri uri = Uri.parse(window.location.href);
+    var port = uri.port != 8080 ? 80 : 9090;
+    Uri url = Uri.parse("http://${uri.host}:${port}/hello/${Uri.encodeQueryComponent(formInput.value)}");
+
     respWrap.classes = ["pure-u-1-1", "well"];
 
-    HttpRequest.request("${serviceUrl}/${formInput.value}", method: "GET").then((response) {
-      formResp.append(new ParagraphElement()..text = "${response.responseText}");
+    HttpRequest.request(url.toString(), method: "GET").then((response) {
+      formResp.append(new ParagraphElement()..text = "${Uri.decodeQueryComponent(response.responseText)}");
     }).catchError((e) {
       formResp.appendHtml("<p>Unable to connect to the server</p>");
     });
